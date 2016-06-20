0a9ae38e54fdf0d81af8cef48b6052dfe2b391b3
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-20 18:43:02 -0400

    initial insert statements

---
+++ CEA_data.sql
@@ -0,0 +1,31 @@
+INSERT INTO departments VALUES ("CSC", "Computer Science");
+INSERT INTO departments VALUES ("BIO", "Biology");
+INSERT INTO departments VALUES ("BCH", "Biochemistry");
+INSERT INTO departments VALUES ("MAT", "Mathemetics");
+INSERT INTO departments VALUES ("CHM", "Chemistry");
+INSERT INTO departments VALUES ("PHY", "Physics");
+INSERT INTO departments VALUES ("STA", "Statistics");
+
+INSERT INTO courses VALUES ("BIO", 120, "science");
+INSERT INTO courses VALUES ("BIO", 130, "science");
+INSERT INTO courses VALUES ("CHM", 138, "science");
+INSERT INTO courses VALUES ("CHM", 139, "science");
+INSERT INTO courses VALUES ("PHY", 131, "science");
+INSERT INTO courses VALUES ("PHY", 132, "science");
+INSERT INTO courses VALUES ("MAT", 135, "science");
+INSERT INTO courses VALUES ("MAT", 136, "science");
+INSERT INTO courses VALUES ("CSC", 120, "science");
+INSERT INTO courses VALUES ("MAT", 223, "science");
+INSERT INTO courses VALUES ("BIO", 230, "science");
+INSERT INTO courses VALUES ("CSC", 148, "science");
+INSERT INTO courses VALUES ("CSC", 165, "science");
+INSERT INTO courses VALUES ("BCH", 210, "science");
+INSERT INTO courses VALUES ("BCH", 311, "science");
+INSERT INTO courses VALUES ("CSC", 207, "science");
+INSERT INTO courses VALUES ("CSC", 209, "science");
+INSERT INTO courses VALUES ("CSC", 236, "science");
+INSERT INTO courses VALUES ("CSC", 263, "science");
+INSERT INTO courses VALUES ("CSC", 258, "science");
+INSERT INTO courses VALUES ("STA", 247, "science");
+
+INSERT INTO students VALUES ("jiananya", "male", 1995, 11, "china", 2014, 09);
